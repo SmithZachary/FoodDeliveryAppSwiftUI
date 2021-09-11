a4478975ba1b9984c64f721aa2de9d6b4cf3f75c
SmithZachary <89857357+SmithZachary@users.noreply.github.com>
2021-09-11 15:59:39 -0500

    Initial Commit

---
+++ FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Zach Smith on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
